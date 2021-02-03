b6727c9402619457be3c832b542a186c7961c063
Andrew Vdovichenko <andrew.vdovichenko@gmail.com>
2021-02-03 22:35:29 +0500

    Создали компонент для главного экрана
    Также вынесли информацию для списка фильмов в отдельный файл const.js

---
+++ src/const.js
@@ -0,0 +1,102 @@
+export const MOVIES = [
+  {
+    title: `Fantastic Beasts: The Crimes of Grindelwald`,
+    posterUrl: `img/fantastic-beasts-the-crimes-of-grindelwald.jpg`,
+    pageUrl: `movie-page.html`,
+  },
+  {
+    title: `Bohemian Rhapsody`,
+    posterUrl: `img/bohemian-rhapsody.jpg`,
+    pageUrl: `movie-page.html`,
+  },
+  {
+    title: `Macbeth`,
+    posterUrl: `img/macbeth.jpg`,
+    pageUrl: `movie-page.html`,
+  },
+  {
+    title: `Aviator`,
+    posterUrl: `img/aviator.jpg`,
+    pageUrl: `movie-page.html`,
+  },
+  {
+    title: `We need to talk about Kevin`,
+    posterUrl: `img/we-need-to-talk-about-kevin.jpg`,
+    pageUrl: `movie-page.html`,
+  },
+  {
+    title: `What We Do in the Shadows`,
+    posterUrl: `img/what-we-do-in-the-shadows.jpg`,
+    pageUrl: `movie-page.html`,
+  },
+  {
+    title: `Revenant`,
+    posterUrl: `img/revenant.jpg`,
+    pageUrl: `movie-page.html`,
+  },
+  {
+    title: `Johnny English`,
+    posterUrl: `img/johnny-english.jpg`,
+    pageUrl: `movie-page.html`,
+  },
+  {
+    title: `Shutter Island`,
+    posterUrl: `img/shutter-island.jpg`,
+    pageUrl: `movie-page.html`,
+  },
+  {
+    title: `Pulp Fiction`,
+    posterUrl: `img/pulp-fiction.jpg`,
+    pageUrl: `movie-page.html`,
+  },
+  {
+    title: `No Country for Old Men`,
+    posterUrl: `img/no-country-for-old-men.jpg`,
+    pageUrl: `movie-page.html`,
+  },
+  {
+    title: `Snatch`,
+    posterUrl: `img/snatch.jpg`,
+    pageUrl: `movie-page.html`,
+  },
+  {
+    title: `Moonrise Kingdom`,
+    posterUrl: `img/moonrise-kingdom.jpg`,
+    pageUrl: `movie-page.html`,
+  },
+  {
+    title: `Seven Years in Tibet`,
+    posterUrl: `img/seven-years-in-tibet.jpg`,
+    pageUrl: `movie-page.html`,
+  },
+  {
+    title: `Midnight Special`,
+    posterUrl: `img/midnight-special.jpg`,
+    pageUrl: `movie-page.html`,
+  },
+  {
+    title: `War of the Worlds`,
+    posterUrl: `img/war-of-the-worlds.jpg`,
+    pageUrl: `movie-page.html`,
+  },
+  {
+    title: `Dardjeeling Limited`,
+    posterUrl: `img/dardjeeling-limited.jpg`,
+    pageUrl: `movie-page.html`,
+  },
+  {
+    title: `Orlando`,
+    posterUrl: `img/orlando.jpg`,
+    pageUrl: `movie-page.html`,
+  },
+  {
+    title: `Mindhunter`,
+    posterUrl: `img/mindhunter.jpg`,
+    pageUrl: `movie-page.html`,
+  },
+  {
+    title: `Midnight Special`,
+    posterUrl: `img/midnight-special.jpg`,
+    pageUrl: `movie-page.html`,
+  },
+];
