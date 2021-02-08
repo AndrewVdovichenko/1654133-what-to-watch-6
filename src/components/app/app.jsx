c6a19f071de58c705cf5eee1bca98b749c9bc566
Andrey Vdovichenko <andrew.vdovichenko@gmail.com>
2021-02-08 12:43:07 +0500

    Подключили маршрутизацию страниц

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,10 +1,44 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {Switch, Route, BrowserRouter} from 'react-router-dom';
 import MainView from '../main-view/main-view';
+import AddReviewView from '../add-review-view/add-review-view';
+import AuthView from '../auth-view/auth-view';
+import MovieView from '../movie-view/movie-view';
+import MyListView from '../my-list-view/my-list-view';
+import NotFoundView from '../not-found-view/not-found-view';
+import PlayerView from '../player-view/player-view';
+import {MOVIES} from '../../const';
+
+const myMovies = MOVIES.slice(0, 5);
 
 const App = (props) => {
   return (
-    <MainView promo={props.promo}/>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <MainView promo={props.promo}/>
+        </Route>
+        <Route exact path="/films/:id/review">
+          <AddReviewView />
+        </Route>
+        <Route exact path="/login">
+          <AuthView />
+        </Route>
+        <Route exact path="/films/:id">
+          <MovieView />
+        </Route>
+        <Route exact path="/mylist">
+          <MyListView myMovies={myMovies}/>
+        </Route>
+        <Route exact path="/player/:id">
+          <PlayerView />
+        </Route>
+        <Route>
+          <NotFoundView />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
