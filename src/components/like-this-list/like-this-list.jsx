d4c183ede85c5b11df8332c14c7f5757cb2e53a2
Andrey Vdovichenko <andrew.vdovichenko@gmail.com>
2021-02-16 17:47:42 +0500

    Создали компонент LikeThisList
    Этот компонент нужен для отображение совпадающих по жанру фильмов на
    странице карточки фильма.

---
+++ src/components/like-this-list/like-this-list.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import MoviesList from '../movies-list/movies-list';
+import {FILMS_PROPS} from '../../const';
+
+const LikeThisList = ({films}) => {
+
+  return (
+    <section className="catalog catalog--like-this">
+      <h2 className="catalog__title">More like this</h2>
+
+      {<MoviesList films={films}/>}
+    </section>
+  );
+};
+
+LikeThisList.propTypes = {
+  films: FILMS_PROPS
+};
+
+export default LikeThisList;
