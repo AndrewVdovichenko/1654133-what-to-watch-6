46bb4534959736fd81dd035be817404aaedc5a42
Andrey Vdovichenko <andrew.vdovichenko@gmail.com>
2021-02-16 17:43:51 +0500

    Мелкие правки
     - В footer захардкодили переход на основную страницу, используя Link;
     - В странице 404 добавили ссылку для перехода на основную страницу.

--- src/components/not-found-view/not-found-view.jsx
+++ src/components/not-found-view/not-found-view.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const NotFoundView = () => {
   return (
     <React.Fragment>
       <h1>Page Not Found</h1>
+      <p>Go to the <Link to="/">main page</Link></p>
     </React.Fragment>
   );
 };
