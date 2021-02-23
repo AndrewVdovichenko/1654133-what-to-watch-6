04a91f681f689d2a808cb7d8b2961085390e35b0
Andrew Vdovichenko <andrew.vdovichenko@gmail.com>
2021-02-23 19:00:55 +0500

    Подключили компонент Видеоплеер к карточке фильма
    При наведении курсора на карточку фильма спустя 1 секунду запускается
    видеоплеер в беззвучном режиме. При уходе курсора с карточки вновь
    отображается картинка. При повторном наведении, воспроизведение начинается
    заново.

--- src/components/small-movie-card/small-movie-card.jsx
+++ src/components/small-movie-card/small-movie-card.jsx
@@ -1,21 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
+import Videoplayer from '../videoplayer/videoplayer';
 import {MOVIE_PROPS} from '../../const';
 
+const PAUSE_BEFORE_AUTOPLAY = 1000;
+
 const SmallMovieCard = (props) => {
+  const [isVideoplayerActive, setVideoplayerActive] = useState(false);
   const {name, previewUrl, id} = props.movie;
+  let timerId = null;
+
+  const handleMouseEnter = () => {
+    props.onHover(id);
+    timerId = setTimeout(() => {
+      setVideoplayerActive(true);
+    }, PAUSE_BEFORE_AUTOPLAY);
+  };
+
+  const handleMouseLeave = () => {
+    props.onHover(-1);
+    clearTimeout(timerId);
+    setVideoplayerActive(false);
+  };
 
   return (
     <article
       className="small-movie-card catalog__movies-card"
-      onMouseEnter={() => {
-        props.onHover(id);
-      }}
-      onMouseLeave={() => {
-        props.onHover(-1);
-      }}>
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}>
       <div className="small-movie-card__image">
-        <img src={previewUrl} alt={name} width="280" height="175" />
+        {isVideoplayerActive
+          ? <Videoplayer movie={props.movie} size={[175, 280]} />
+          : <img src={previewUrl} alt={name} width="280" height="175" />}
       </div>
       <h3 className="small-movie-card__title">
         <a className="small-movie-card__link" href={`/films/${id}`}>{name}</a>
