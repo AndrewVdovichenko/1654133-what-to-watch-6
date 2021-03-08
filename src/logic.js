b683340d979e5b3bf504ccdad831bc1a48493a1c
andrey vdovichenko <andrew.vdovichenko@gmail.com>
2021-03-08 09:35:47 +0500

    Настроили адаптер для данных с сервера

--- src/logic.js
+++ src/logic.js
@@ -19,3 +19,33 @@ export const getSortedFilmsByGenre = (films, genre) => {
 
   return films.filter((film) => film.genre === genre);
 };
+
+export const adaptToClient = (film) => {
+  const adaptedFilm = Object.assign(
+      {},
+      film,
+      {
+        posterUrl: film.poster_image,
+        previewUrl: film.preview_image,
+        backgroundUrl: film.background_image,
+        backgroundColor: film.background_color,
+        videoLink: film.video_link,
+        previewVideoLink: film.preview_video_link,
+        scoresCount: film.scores_count,
+        runTime: film.run_time,
+        isFavorite: film.is_favorite,
+      }
+  );
+
+  delete adaptedFilm.poster_image;
+  delete adaptedFilm.preview_image;
+  delete adaptedFilm.background_image;
+  delete adaptedFilm.background_color;
+  delete adaptedFilm.video_link;
+  delete adaptedFilm.preview_video_link;
+  delete adaptedFilm.scores_count;
+  delete adaptedFilm.run_time;
+  delete adaptedFilm.is_favorite;
+
+  return adaptedFilm;
+};
--- src/store/api-actions.js
+++ src/store/api-actions.js
@@ -1,14 +1,17 @@
 import {ActionCreator} from './action';
 import {AuthorizationStatus} from '../utils';
+import {adaptToClient} from '../logic';
 
 export const fetchFilmsList = () => (dispatch, _getState, api) => (
   api.get(`/films`)
-    .then(({data}) => dispatch(ActionCreator.loadFilms(data)))
+    .then(({data}) => data.map((value) => adaptToClient(value)))
+    .then((data) => dispatch(ActionCreator.loadFilms(data)))
 );
 
 export const fetchPromoMovie = () => (dispatch, _getState, api) => (
   api.get(`/films/promo`)
-    .then(({data}) => dispatch(ActionCreator.loadPromo(data)))
+    .then(({data}) => adaptToClient(data))
+    .then((data) => dispatch(ActionCreator.loadPromo(data)))
 );
 
 export const checkAuth = () => (dispatch, _getState, api) => (
