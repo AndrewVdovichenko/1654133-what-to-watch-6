fe80652f4a1b507315b7ce721031e2076829ad82
andrey vdovichenko <andrew.vdovichenko@gmail.com>
2021-03-08 19:00:28 +0500

    Fix: почистили AuthView после проверки линтером

--- src/components/auth-view/auth-view.jsx
+++ src/components/auth-view/auth-view.jsx
@@ -37,22 +37,22 @@ const AuthView = ({onSubmit}) => {
             <div className="sign-in__field">
               <input
                 ref={loginRef}
-                className="sign-in__input" 
-                type="email" 
-                placeholder="Email address" 
-                name="user-email" 
-                id="user-email"  
+                className="sign-in__input"
+                type="email"
+                placeholder="Email address"
+                name="user-email"
+                id="user-email"
               />
               <label className="sign-in__label visually-hidden" htmlFor="user-email">Email address</label>
             </div>
             <div className="sign-in__field">
               <input
                 ref={passwordRef}
-                className="sign-in__input" 
-                type="password" 
-                placeholder="Password" 
-                name="user-password" 
-                id="user-password" 
+                className="sign-in__input"
+                type="password"
+                placeholder="Password"
+                name="user-password"
+                id="user-password"
               />
               <label className="sign-in__label visually-hidden" htmlFor="user-password">Password</label>
             </div>
