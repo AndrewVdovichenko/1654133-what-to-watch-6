5936398577592604126ed9401e3f9fb998526f4c
andrey vdovichenko <andrew.vdovichenko@gmail.com>
2021-03-08 18:57:58 +0500

    Создали middleware для редиректа

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import {reducer} from './store/reducer';
 import {ActionCreator} from './store/action';
 import {checkAuth, fetchFilmsList, fetchPromoMovie} from "./store/api-actions";
 import {AuthorizationStatus} from "./utils";
+import {redirect} from './store/middlewares/redirect';
 
 const api = createAPI(
     () => store.dispatch(ActionCreator.requireAuthorization(AuthorizationStatus.NO_AUTH))
@@ -18,7 +19,8 @@ const api = createAPI(
 const store = createStore(
     reducer,
     composeWithDevTools(
-        applyMiddleware(thunk.withExtraArgument(api))
+        applyMiddleware(thunk.withExtraArgument(api)),
+        applyMiddleware(redirect)
     ),
 );
 
