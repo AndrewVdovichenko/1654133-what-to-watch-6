a2e7f1b4bc8267f3d865b02756b486d421754bae
Andrew Vdovichenko <andrew.vdovichenko@gmail.com>
2021-03-14 12:25:19 +0500

    Избавились от ActionCreator
    ActionCreator разбили на отдельные функции.

--- src/store/action.js
+++ src/store/action.js
@@ -9,42 +9,41 @@ export const ActionType = {
   REDIRECT_TO_ROUTE: `app/redirectToRoute`,
 };
 
-export const ActionCreator = {
-  selectGenre: (genre) => ({
-    type: ActionType.SELECT_GENRE,
-    payload: genre,
-  }),
+export const selectGenre = (genre) => ({
+  type: ActionType.SELECT_GENRE,
+  payload: genre,
+});
 
-  getFilms: () => ({
-    type: ActionType.GET_FILMS,
-  }),
+export const getFilms = () => ({
+  type: ActionType.GET_FILMS,
+});
 
-  loadFilms: (films) => ({
-    type: ActionType.LOAD_FILMS,
-    payload: films,
-  }),
 
-  loadPromo: (promo) => ({
-    type: ActionType.LOAD_PROMO,
-    payload: promo,
-  }),
+export const loadFilms = (films) => ({
+  type: ActionType.LOAD_FILMS,
+  payload: films,
+});
 
-  requireAuthorization: (status) => ({
-    type: ActionType.REQUIRED_AUTHORIZATION,
-    payload: status,
-  }),
+export const loadPromo = (promo) => ({
+  type: ActionType.LOAD_PROMO,
+  payload: promo,
+});
 
-  showMore: () => ({
-    type: ActionType.SHOW_MORE,
-  }),
+export const requireAuthorization = (status) => ({
+  type: ActionType.REQUIRED_AUTHORIZATION,
+  payload: status,
+});
 
-  loadUserInfo: (userInfo) => ({
-    type: ActionType.LOAD_USER_INFO,
-    payload: userInfo,
-  }),
+export const showMore = () => ({
+  type: ActionType.SHOW_MORE,
+});
 
-  redirectToRoute: (url) => ({
-    type: ActionType.REDIRECT_TO_ROUTE,
-    payload: url,
-  }),
-};
+export const loadUserInfo = (userInfo) => ({
+  type: ActionType.LOAD_USER_INFO,
+  payload: userInfo,
+});
+
+export const redirectToRoute = (url) => ({
+  type: ActionType.REDIRECT_TO_ROUTE,
+  payload: url,
+});
--- src/store/api-actions.js
+++ src/store/api-actions.js
@@ -1,24 +1,24 @@
-import {ActionCreator} from './action';
+import {loadFilms, loadPromo, requireAuthorization, loadUserInfo, redirectToRoute} from './action';
 import {AuthorizationStatus} from '../utils/const';
 import {adaptToClient, adaptUserInfoToClient} from '../utils/helpers';
 
 export const fetchFilmsList = () => (dispatch, _getState, api) => (
   api.get(`/films`)
     .then(({data}) => data.map((value) => adaptToClient(value)))
-    .then((data) => dispatch(ActionCreator.loadFilms(data)))
+    .then((data) => dispatch(loadFilms(data)))
 );
 
 export const fetchPromoMovie = () => (dispatch, _getState, api) => (
   api.get(`/films/promo`)
     .then(({data}) => adaptToClient(data))
-    .then((data) => dispatch(ActionCreator.loadPromo(data)))
+    .then((data) => dispatch(loadPromo(data)))
 );
 
 export const checkAuth = () => (dispatch, _getState, api) => (
   api.get(`/login`)
     .then(({data}) => {
-      dispatch(ActionCreator.requireAuthorization(AuthorizationStatus.AUTH));
-      dispatch(ActionCreator.loadUserInfo(adaptUserInfoToClient(data)));
+      dispatch(requireAuthorization(AuthorizationStatus.AUTH));
+      dispatch(loadUserInfo(adaptUserInfoToClient(data)));
     })
     .catch(() => {})
 );
@@ -27,8 +27,8 @@ export const login = ({login: email, password}) => (dispatch, _getState, api) =>
   api.post(`/login`, {email, password})
     .then(({data}) => {
       const user = adaptUserInfoToClient(data);
-      dispatch(ActionCreator.requireAuthorization(AuthorizationStatus.AUTH));
-      dispatch(ActionCreator.loadUserInfo(user));
+      dispatch(requireAuthorization(AuthorizationStatus.AUTH));
+      dispatch(loadUserInfo(user));
     })
-    .then(() => dispatch(ActionCreator.redirectToRoute(`/`)))
+    .then(() => dispatch(redirectToRoute(`/`)))
 );
